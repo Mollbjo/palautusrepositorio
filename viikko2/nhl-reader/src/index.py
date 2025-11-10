5e8fb99d59e450756396c7ce4f8bd53e1df0a8d2
Mollbjo <joonamollberg@gmail.com>
2025-11-10 17:49:37 +0200

    nhl pelaajalista

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,60 @@
+import requests
 from player import Player
+from rich.table import Table
+from rich.console import Console
 
 def main():
-    pass
+    season = input("Enter NHL season (example: 2024-25): ")
+    nationality = input("Enter nationality code (example: FIN): ")
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality(nationality)
+
+    console = Console()
+    table = Table(title=f"Top NHL Players from {nationality} ({season})")
+    table.add_column("Name", style="cyan")
+    table.add_column("Team", style="magenta")
+    table.add_column("Games", justify="right")
+    table.add_column("Goals", justify="right")
+    table.add_column("Assists", justify="right")
+    table.add_column("Points", justify="right")
+
+    for player in players:
+        table.add_row(
+            player.name,
+            player.team,
+            str(player.games),
+            str(player.goals),
+            str(player.assists),
+            str(player.points)
+        )
+    console.print(table)
+
+class PlayerReader:
+    def __init__ (self, url):
+        self.url = url
+    
+    def get_players(self):
+        response = requests.get(self.url).json()
+        print("JSON-muotoinen vastaus:")
+        print(response)
+
+        players = []
+
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        return players
+    
+class PlayerStats:
+    def __init__ (self, reader: PlayerReader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality: str):
+        nationality_filter = [player for player in self.players if player.nationality == nationality]
+        nationality_filter.sort(key=lambda player: player.points, reverse=True)
+        return nationality_filter
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.nationality = dict["nationality"]
+        self.assists = dict["assists"]
+        self.goals = dict["goals"]
+        self.team = dict["team"]
+        self.games = dict["games"]
+        self.id = dict["id"]
+        self.points = self.goals + self.assists
+
     def __str__(self):
-        return self.name
+        return (f" {self.name:20} {self.team:20} {self.goals} + {self.assists} = {self.points}")
